b7004a506d21cecd9e4de4ec010bb41463fabca3
SrDev-Henrique <halbuquerque2850@gmail.com>
2025-03-03 04:13:57 -0300

    Banco de dados - Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,19 +1,39 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
-    #database = {}
+  #database = {};
 
-    select(table) {
-        const data = this.#database[table] ?? []
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
 
-        return data
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
     }
 
-    insert(table, data) {
-        if (Array.isArray(this.#database[table])) {
-            this.#database[table].push(data)
-        } else {
-            this.#database[table] = [data]
-        }
+    this.#persist();
 
-        return data
-    }
+    return data;
+  }
 }
